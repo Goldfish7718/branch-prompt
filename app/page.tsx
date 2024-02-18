6ffb923f9d6c9db26fe98e9507a7a39b4c230c56
Tejas Nanoti <tejasnanoti2@gmail.com>
2024-02-19 01:21:52 +0530

    style[app/page]:💅 Complete Header;

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { ArrowUpRightFromSquare } from "lucide-react";
 
 export default function Home() {
   return (
     <main>
-      
+      <div id="header" className="text-center">
+        <h1 className="font-extrabold mt-40">
+          Branch-prompt
+        </h1>
+
+        <h4 className="text-gray-300 font-light my-6 text-lg sm:text-xl">
+          Where's all the gossip? The Secrets? It's here.<br /> Share all your secrets, confessions and gossip here <span className="font-extrabold">anonymously</span> at Branch-prompt <br />
+        </h4>
+
+        <h4 className="font-extralight text-gray-400">Exclusive to <span className="italic">KKWIEER</span> Campus and students*</h4>
+
+        <div className="flex flex-col sm:flex-row items-center justify-center my-20">
+          <h4>You can have one of these too!</h4>
+          <Button variant="link">Create new prompt <ArrowUpRightFromSquare size={18} className="mx-1" /></Button>
+        </div>
+      </div>
     </main>
   );
 }

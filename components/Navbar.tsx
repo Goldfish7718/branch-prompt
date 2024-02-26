3a19432db243bb51ac4a7e8a141505e3b84e252c
Tejas Nanoti <tejasnanoti2@gmail.com>
2024-02-26 19:51:37 +0530

    style[banner]:💅 Add Banner;

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import { GitBranch, Menu, Plus } from "lucide-react"
+import { Cross, GitBranch, Info, Menu, Plus, X } from "lucide-react"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 import { Button } from "./ui/button"
 import { ModeToggle } from "./mode-toggle"
 import NewPromptTrigger from "./NewPrompt"
+import { useState } from "react"
 
 const Navbar = () => {
 
@@ -15,34 +16,43 @@ const Navbar = () => {
         })
     }
 
+    const [bannerOpen, setBannerOpen] = useState(true)
+
   return (
-    <nav className="p-4 py-6 sm:p-3 sm:mt-0 sm:text-left z-10 fixed top-0 flex justify-between items-center w-full dark:bg-[#0a0a0a] dark:bg-opacity-70 backdrop-blur-sm">
-        <div className="flex flex-row items-center">
-            <h3 onClick={() => scrollToHeader()} className="hover:cursor-pointer">Branch-Prompt</h3>
-            <GitBranch size={24} className="mx-2" />
+    <nav className="z-10 fixed top-0  w-full">
+        <div id="banner" className={`p-2 bg-red-600 flex items-center justify-center ${!bannerOpen && 'hidden'}`}>
+            <Info size={18} className="mx-1 text-white ml-auto" />
+            <h4 className="text-sm font-light text-white mr-auto">This website only has one maintainer. Please do not spam the feeds with useless prompts. Thank you!</h4>
+            <X size={18} className="text-white mx-2 hover:cursor-pointer" onClick={() => setBannerOpen(false)} />
         </div>
+        <div className="flex p-4 py-6 sm:p-3 sm:mt-0 sm:text-left justify-between items-center dark:bg-[#0a0a0a] dark:bg-opacity-70 backdrop-blur-sm">
+            <div className="flex flex-row items-center">
+                <h3 onClick={() => scrollToHeader()} className="hover:cursor-pointer">Branch-Prompt</h3>
+                <GitBranch size={24} className="mx-2" />
+            </div>
 
-        {/* MOBILE NAVIGATION */}
-        <Sheet>
-            <SheetTrigger className="sm:hidden">
-                <Menu />
-            </SheetTrigger>
-            <SheetContent>
-                <div className="my-5 flex flex-col justify-center">
-                    <NewPromptTrigger>
-                        <Button variant='outline' className="m-2 w-full">New Prompt <Plus size={18} className="mx-1" /></Button>
-                    </NewPromptTrigger>
-                    <ModeToggle className="w-full" />
-                </div>
-            </SheetContent>
-        </Sheet>
+            {/* MOBILE NAVIGATION */}
+            <Sheet>
+                <SheetTrigger className="sm:hidden">
+                    <Menu />
+                </SheetTrigger>
+                <SheetContent>
+                    <div className="my-5 flex flex-col justify-center">
+                        <NewPromptTrigger>
+                            <Button variant='outline' className="m-2 w-full">New Prompt <Plus size={18} className="mx-1" /></Button>
+                        </NewPromptTrigger>
+                        <ModeToggle className="w-full" />
+                    </div>
+                </SheetContent>
+            </Sheet>
 
-        {/* DESKTOP NAVIGATION */}
-        <div className="flex-row hidden sm:flex items-center">
-            <NewPromptTrigger>
-                <Button variant='outline' className="mx-2">New Prompt <Plus size={18} className="mx-1" /></Button>
-            </NewPromptTrigger>
-            <ModeToggle />
+            {/* DESKTOP NAVIGATION */}
+            <div className="flex-row hidden sm:flex items-center">
+                <NewPromptTrigger>
+                    <Button variant='outline' className="mx-2">New Prompt <Plus size={18} className="mx-1" /></Button>
+                </NewPromptTrigger>
+                <ModeToggle />
+            </div>
         </div>
     </nav>
   )

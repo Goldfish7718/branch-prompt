01008a0bea331b7cf7bc9a5c2873d7aad2724b57
Tejas Nanoti <tejasnanoti2@gmail.com>
2024-06-24 22:49:00 +0530

    Update mode toggle to be a switch

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,44 +1,28 @@
-"use client"
-
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { useTheme } from "@/components/theme-provider"
 
-interface ModeToggleProps {
-  className?: string;
-}
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme()
 
-export function ModeToggle({ className }: ModeToggleProps) {
-  const { setTheme } = useTheme()
+  const handleTheme = () => {
+    if (theme == 'light') setTheme('dark')
+    else setTheme('light')
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className={`m-2 ${className}`}>
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant='outline' onClick={handleTheme}>
+      {theme == 'light' ? 
+        <>
+          <span>Sunrise</span>
+          <Sun size={18} className="mx-1" />
+        </> : 
+        <>
+          <span>After hours</span>
+          <Moon size={18} className="mx-1" />
+        </>
+      }
+    </Button>
   )
 }
